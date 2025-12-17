82781463f2eed98fd5a01075e35976f3290b028a
bala-amypo <bala@amypo.in>
2025-12-17 18:14:21 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,29 +1,19 @@
 package com.example.demo.service.impl;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
 
-import com.example.demo.model.Ticket;
-import com.example.demo.repository.TicketRepository;
-import com.example.demo.service.TicketService;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
 
 @Service
-public class TicketServiceImpl implements TicketService {
-    private final TicketRepository ticketRepository;
-    TicketServiceImpl(TicketRepository ticketRepository){
-        this.ticketRepository=ticketRepository;
-    }
-    public Ticket createTicket(Ticket ticket){
-        return ticketRepository.save(ticket);
-    }
-    public List<Ticket> getAllTickets(){
-        return ticketRepository.findAll();
-    }
-    public Ticket getTicket(Long id){
-        Optional<Ticket>opTicket= ticketRepository.findById(id);
-        return opTicket.orElse(null);
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    UserServiceImpl(UserRepository userRepository){
+        this.userRepository=userRepository;
     }
     
+    public User register(User user){
+        return userRepository.save(user);
+    }
 }
