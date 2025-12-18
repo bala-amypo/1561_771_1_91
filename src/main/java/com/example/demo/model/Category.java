a439d949a021f6e222165bb13873b730d85d4982
bala-amypo <bala@amypo.in>
2025-12-18 05:05:53 +0000

    User commit

--- src/main/java/com/example/demo/model/Category.java
+++ src/main/java/com/example/demo/model/Category.java
@@ -1,10 +1,67 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="categories")
 public class Category {
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true,nullable = false)
+    private String categoryName;
+    private String description;
+    @Column(nullable = false)
+    private String defaultUrgency;
+    private LocalDateTime createdAt;
+    @PrePersist
+    protected void onCreated(){
+        this.createdAt = LocalDateTime.now();
+    }
+    public Category() {
+    }
+    public Category(String categoryName, String description, String defaultUrgency) {
+        this.categoryName = categoryName;
+        this.description = description;
+        this.defaultUrgency = defaultUrgency;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getCategoryName() {
+        return categoryName;
+    }
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getDefaultUrgency() {
+        return defaultUrgency;
+    }
+    public void setDefaultUrgency(String defaultUrgency) {
+        this.defaultUrgency = defaultUrgency;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    
 }
