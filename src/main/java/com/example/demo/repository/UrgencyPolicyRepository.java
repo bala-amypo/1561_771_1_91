82ba98a84229356f7007e104b5a7a7737635ee65
bala-amypo <bala@amypo.in>
2025-12-23 08:58:45 +0000

    User commit

--- src/main/java/com/example/demo/repository/UrgencyPolicyRepository.java
+++ src/main/java/com/example/demo/repository/UrgencyPolicyRepository.java
@@ -1,12 +1,12 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.UrgencyPolicy;
 
-@Repository
-public interface UrgencyPolicyRepository extends JpaRepository<UrgencyPolicy,Long> {
+public interface UrgencyPolicyRepository extends JpaRepository<UrgencyPolicy, Long> {
 
-    
-} 
+    List<UrgencyPolicy> findByKeywordContainingIgnoreCase(String keyword);
+}
