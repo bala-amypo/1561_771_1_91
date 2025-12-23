c6758c32f6dec2750cd003ce9987afb3d5772405
bala-amypo <bala@amypo.in>
2025-12-23 08:54:58 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
+++ src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
@@ -3,12 +3,10 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.CategorizationRule;
 
-@Repository
-public interface CategorizationRuleRepository extends JpaRepository<CategorizationRule,Long> {
-    List<CategorizationRule> findByCategoryId(Long categoryId);
-    
+public interface CategorizationRuleRepository extends JpaRepository<CategorizationRule, Long> {
+
+    List<CategorizationRule> findByKeywordContainingIgnoreCase(String keyword);
 }
--- src/main/java/com/example/demo/repository/TicketRepository.java
+++ src/main/java/com/example/demo/repository/TicketRepository.java
@@ -1,12 +1,8 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Ticket;
 
-@Repository
-public interface TicketRepository extends JpaRepository<Ticket,Long> {
-
-    
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
 }
